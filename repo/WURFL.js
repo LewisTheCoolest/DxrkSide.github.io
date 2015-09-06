8beb8d193a95e235d70f2e7aec0eef21a8e0039e
BannerBomb <BannerBomb55@gmail.com>
2015-09-06 13:03:03 -0400

    https://github.com/BannerBomb/BannerBomb.github.io/repo

---
+++ repo/WURFL.js
@@ -0,0 +1,6 @@
+<script type='text/javascript' src="//wurfl.io/wurfl.js"></script>
+console.log(WURFL);
+if(WURFL.is_mobile){
+	//dostuff();
+}
+is_mobile":true,"complete_device_name":"Apple iPod Touch","form_factor":"Other Mobile"}
