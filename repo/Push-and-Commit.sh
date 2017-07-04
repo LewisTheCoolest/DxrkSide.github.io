03f7aa78b5cfef72e876e1891cc877bcfb997c1f
BannerBomb <BannerBomb55@gmail.com>
2017-07-04 04:11:29 +0000

    https://github.com/BannerBomb/BannerBomb.github.io/repo

---
+++ repo/Push-and-Commit.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+sleep .5
+git add -A
+sleep .5
+echo 'Do NOT FORGET TO CHANGE .git/config'
+echo 'url = git://github.com/BannerBomb/BannerBomb.github.io.git'
+echo 'to url = https://github.com/BannerBomb/BannerBomb.github.io.git'
+echo 'after Cloning the repo with git://github.com/BannerBomb/BannerBomb.github.io.git'
+echo ' '
+echo 'Continuing in 5...'
+sleep 5
+git commit -m "https://github.com/BannerBomb/BannerBomb.github.io/repo"
+sleep .3
+git push -u origin master
+sleep .6
+git push
+sleep .5
+exit
