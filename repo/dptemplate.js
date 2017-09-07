abcff41ffecf6f58ee23cc0cf038aeed35911f6b
BannerBomb <BannerBomb55@gmail.com>
2017-09-06 21:58:51 -0400

    Update dptemplate.js

--- repo/dptemplate.js
+++ repo/dptemplate.js
@@ -241,6 +241,8 @@ var template = ' \
   <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> \
   <html> \
     <head> \
+      <link rel="stylesheet" type="text/css" href="../../new-depiction/depiction.css" /> \
+      <script type="text/javascript" src="../../new-depiction/depiction.css"></script> \
       <meta http-equiv="content-type" content="text/html" charset="utf-8"/> \
       <meta charset="utf-8"> \
       <link rel="shortcut icon" href=""> \
